6e88f31f6a89cc50b69deb4dc185188303338db7
mgty <marygracety@gmail.com>
2020-02-10 12:57:13 -0500

    add validation and check for empty variables

--- checkout.php
+++ checkout.php
@@ -5,8 +5,21 @@
     $quantity = filter_input(INPUT_POST, 'unit_price', FILTER_VALIDATE_INT);
     
     // validate price
-   
+   if ($unit_price === FALSE){          #if unit price is empty
+    $error_message = "Price must be a valid number";
+   } else if ($unit_price <0){
+    $error_message = "Price must be more than 0";
+   }
     
+   //validate quantity
+   else if ($quantity === FALSE){       #if quantity price is empty
+    $error_message = "Quantity must be a valid number";
+   }else if($quantity <0) {
+    $error_message = "Quantity must be more than 0";
+   } else {
+    $error_message = ''; 
+   }
+
 
     // if an error message exists, go to the index page
     
