419b3e2f5296bc51d7431bd82e448782e28a7839
mgty <marygracety@gmail.com>
2020-02-10 12:53:14 -0500

    get variables from index file

---
+++ checkout.php
@@ -0,0 +1,20 @@
+<?php
+    // get the data from the form
+    $description = filter_input(INPUT_POST, 'description');
+    $unit_price = filter_input(INPUT_POST, 'unit_price', FILTER_VALIDATE_FLOAT);
+    $quantity = filter_input(INPUT_POST, 'unit_price', FILTER_VALIDATE_INT);
+    
+    // validate price
+   
+    
+
+    // if an error message exists, go to the index page
+    
+     // calculate the sub total, sales tax and total
+    
+  
+
+
+    // apply currency and percent formatting  
+    
+?>
